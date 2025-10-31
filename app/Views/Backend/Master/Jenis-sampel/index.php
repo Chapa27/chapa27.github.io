61c7b82b0cfe12cb5cbfeb75abaa802479654b58
chapa27 <panuturanchan@gmail.com>
2025-10-31 23:26:05 +0700

    second commit

---
+++ app/Views/Backend/Master/Jenis-sampel/index.php
@@ -0,0 +1,90 @@
+<?= $this->extend('Layout/_main'); ?>
+<?= $this->section('topAssets'); ?>
+<link rel="stylesheet" href="<?= base_url('assets/css/plugins/dataTables.bootstrap5.css'); ?>">
+<?= $this->endSection(); ?>
+<?= $this->section('content'); ?>
+<div class="pc-container">
+    <div class="pc-content">
+        <!-- [ breadcrumb ] start -->
+        <div class="page-header">
+            <div class="page-block">
+                <div class="row align-items-center">
+                    <div class="col-md-12">
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="javascript: void(0)">Master Data</a></li>
+                            <li class="breadcrumb-item"><a href="#"><?= $title; ?></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- [ breadcrumb ] end -->
+
+        <!-- [ Main Content ] start -->
+        <div class="row">
+            <!-- [ sample-page ] start -->
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4><span class="pc-micon"><i class="ti ti-list"></i></span><?= $title; ?></h4>
+                        <div class="d-flex justify-content-end align-items-center gap-1">
+                            <?= $this->include('Componen/_btn-refresh'); ?>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary btn-sm btn-tambah">
+                                <span class="pc-micon"><i class="ti ti-circle-plus"></i> Tambah Data
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="view-data"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- [ sample-page ] end -->
+        </div>
+        <!-- [ Main Content ] end -->
+    </div>
+</div>
+<div class="view-modal" style="display: none;"></div>
+<?= $this->endSection(); ?>
+
+<?= $this->section('bottomAssets'); ?>
+<script src="<?= base_url(); ?>assets/js/plugins/datatables/dataTables.2.3.4.js"></script>
+<script src="<?= base_url('assets/js/plugins/datatables/dataTables.bootstrap5.js'); ?>"></script>
+<script src="<?= base_url('assets/js/plugins/datatables/dataTables.responsive.js'); ?>"></script>
+<script src="<?= base_url('assets/js/plugins/sweetalert2.all.min.js'); ?>"></script>
+
+<script>
+    function listData() {
+        $.ajax({
+            url: "<?= site_url('master-data/jenis-sampel/list-data'); ?>",
+            dataType: 'json',
+            success: function(response) {
+                $(".view-data").html(response.data);
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(xhr.status + ' ' + xhr.responseText + ' ' + thrownError);
+            }
+        })
+    }
+
+    $(document).ready(function() {
+        listData();
+
+        $(".btn-tambah").click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: "<?= site_url('master-data/jenis-sampel/add-data'); ?>",
+                dataType: 'json',
+                success: function(response) {
+                    $(".view-modal").html(response.data).show();
+                    $("#addData").modal('show');
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                }
+            })
+        })
+    })
+</script>
+<?= $this->endSection(); ?>
