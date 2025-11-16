21e9a3c4600eb593bc6be815e17ff47dece3b391
chapa27 <panuturanchan@gmail.com>
2025-11-16 09:55:37 +0700

    secondCommit

---
+++ app/Database/Migrations/2025-11-16-025228_LaboratoriumMaster.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class LaboratoriumMaster extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama_lab' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'lantai' => [
+                'type'       => 'INT'
+            ],
+            'is_active' => [
+                'type'  => 'BOOLEAN',
+                'default' => 1
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp',
+            'created_by'     => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100'
+            ],
+            'updated_by'     => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100'
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('master_laboratorium');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('master_laboratorium');
+    }
+}
