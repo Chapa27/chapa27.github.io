0603a233fcf8ec47a3a4fb7fb7f136ca2791f57b
chapa27 <panuturanchan@gmail.com>
2025-11-22 06:37:47 +0700

    secondCommit

---
+++ app/Views/Backend/Layout copy/_main.php
@@ -0,0 +1,22 @@
+<!-- [ Top ] start -->
+<?= $this->include('Backend/Layout/_top'); ?>
+<!-- [ Top ] end -->
+
+<!-- [ Sidebar Menu ] start -->
+<?= $this->include('Backend/Layout/_navbar'); ?>
+<!-- [ Sidebar Menu ] end -->
+
+<!-- [ Header Topbar ] start -->
+<?= $this->include('Backend/Layout/_header'); ?>
+<!-- [ Header ] end -->
+
+<!-- [ Main Content ] start -->
+<?= $this->renderSection('content', true); ?>
+<!-- [ Main Content ] end -->
+
+<!-- [ Footer ] start -->
+<?= $this->include('Backend/Layout/_footer'); ?>
+<!-- [ Footer ] end -->
+
+<!-- Required Js -->
+<?= $this->include('Backend/Layout/_bottom'); ?>
---
+++ app/Views/Backend/Layout copy/_navbar.php
@@ -0,0 +1,97 @@
+<nav class="pc-sidebar">
+    <div class="navbar-wrapper">
+        <div class="m-header bg-teal-100">
+            <a href="#" class="b-brand text-primary">
+                <!-- ========   Change your logo from here   ============ -->
+                <img src="<?= base_url('img/bblkm-jakarta.png'); ?>" class="img-fluid" alt="logo" style="height: 55px;">
+            </a>
+        </div>
+        <div class="navbar-content">
+            <ul class="pc-navbar">
+                <li class="pc-item">
+                    <a href="<?= base_url('home/dashboard'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-home"></i></span>
+                        <span class="pc-mtext">Home</span>
+                    </a>
+                </li>
+                <li class="pc-item pc-caption">
+                    <label>Modul Pelayanan Sampel</label>
+                    <i class="ti ti-dashboard"></i>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('pelayanan-sampel/permintaan'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-arrow-right"></i></span>
+                        <span class="pc-mtext">Permintaan</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('pelayanan-sampel/penawaran'); ?>" class="pc-link" onclick="showLoading()">
+                        <span class="pc-micon"><i class="fa-solid fa-arrow-right"></i></span>
+                        <span class="pc-mtext">Penawaran</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('pelayanan-sampel/biaya-pengujian-sampel'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-arrow-right"></i></span>
+                        <span class="pc-mtext">Biaya pengujian sampel</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('pelayanan-sampel/pengantar-lhu'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-arrow-right"></i></span>
+                        <span class="pc-mtext">Pengantar LHU</span>
+                    </a>
+                </li>
+                <li class="pc-item pc-caption">
+                    <label>Master Data</label>
+                    <i class="ti ti-databases"></i>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('master-data/pelanggan'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-database"></i></span>
+                        <span class="pc-mtext">Pelanggan</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('master-data/laboratorium'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-database"></i></span>
+                        <span class="pc-mtext">Laboratorium</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('master-data/jenis-sampel'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-database"></i></span>
+                        <span class="pc-mtext">Jenis Sampel</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('master-data/biaya-akomodasi'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-database"></i></span>
+                        <span class="pc-mtext">Biaya Akomodasi</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('master-data/instansi'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-database"></i></span>
+                        <span class="pc-mtext">Instansi</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('master-data/penyakit'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-database"></i></span>
+                        <span class="pc-mtext">Penyakit</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="<?= base_url('master-data/coolbox'); ?>" class="pc-link">
+                        <span class="pc-micon"><i class="fa-solid fa-database"></i></span>
+                        <span class="pc-mtext">Coolbox</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="card text-center">
+                <!-- Blank -->
+            </div>
+        </div>
+    </div>
+</nav>
