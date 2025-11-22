97a1ec000fe66a5c17aaae06a645b67c070fdcc1
chapa27 <panuturanchan@gmail.com>
2025-11-22 07:06:05 +0700

    secondCommit

--- app/Views/Backend/Layout/_top.php
+++ app/Views/Backend/Layout/_top.php
@@ -4,12 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Mantis is made using Bootstrap 5 design framework. Download the free admin template & use it for your project.">
-    <meta name="keywords" content="Mantis, Dashboard UI Kit, Bootstrap 5, Admin Template, Admin Dashboard, CRM, CMS, Bootstrap Admin Template">
+    <meta name="description" content="BBLKM Jakarta Aplikasi Sistem Informasi">
+    <meta name="keywords" content="BB Labkesmas Jakarta Aplikasi">
     <meta name="author" content="CodedThemes">
-    <title>Document</title>
+    <!-- [Favicon] icon -->
+    <link rel="icon" href="<?= base_url('assets/images/logo-bblabkes-jkt.svg'); ?>" type="image/x-icon"> <!-- [Google Font] Family -->
+    <link rel="stylesheet" href="<?= base_url('assets/fonts/css2.css'); ?>" id="main-font-link">
+    <!-- [Tabler Icons] https://tablericons.com -->
+    <link rel="stylesheet" href="<?= base_url('assets/fonts/tabler-icons.min.css'); ?>" >
+    <!-- [Feather Icons] https://feathericons.com -->
+    <link rel="stylesheet" href="<?= base_url('assets/fonts/feather.css'); ?>" >
+    <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
+    <link rel="stylesheet" href="<?= base_url('assets/fonts/fontawesome.css'); ?>" >
+    <!-- [Material Icons] https://fonts.google.com/icons -->
+    <link rel="stylesheet" href="<?= base_url('assets/fonts/material.css'); ?>" >
+    <!-- [Template CSS Files] -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>" id="main-style-link" >
+    <link rel="stylesheet" href="<?= base_url('assets/css/style-preset.css'); ?>" >
 </head>
-<body>
-    
-</body>
-</html>
+<!-- [Head] end -->
+<!-- [Body] Start -->
+
+<body data-pc-preset="preset-1" data-pc-direction="ltr" data-pc-theme="light">
+    <div class="loader-bg">
+        <div class="loader-track">
+            <div class="loader-fill"></div>
+        </div>
+    </div>
